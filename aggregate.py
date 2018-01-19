6345fd0c65b4c2d824657e82f67497712a77d0b9
Ben Lewis <ben.lewis@ga.gov.au>
2018-01-19 15:47:24 +1100

    starting to automate

--- aggregate.py
+++ aggregate.py
@@ -21,25 +21,122 @@
    others may not after aggregation.
 
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
+def wofs_ma(stack):
+    stack.water.data &= ~np.uint8(4) # remove ocean masking
+
+    wet = stack.water == 128
+    dry = stack.water == 0
+    clear = wet | dry
+
+    masked = np.ma.array(wet.data, mask=~clear.data)
+
+    return masked, stack.time
+
 class AggregateDynamics:
     pixels = None
     timeaxis = None
     upper = None
     lower = None
     bestestimate = None
-    def __init__(self, data, time):
+    def __init__(self, stack):
+        stack.water.data &= ~np.uint8(4) # remove ocean masking
+
+        wet = stack.water == 128
+        dry = stack.water == 0
+        clear = wet | dry
+
+        self.masked = np.ma.array(wet.data, mask=~clear.data)
+        t,x,y = self.masked.shape
+        self.pixels = x*y
+        self.timeaxis = stack.time.data
+        self.compute()
+        if self.bestestimate is None:
+            self.bestestimate = 0.5 * (self.lower + self.upper)
+    def compute(self):
         raise NotImplementedError
     def plot(self, axes=None):
         ax = axes or plt.gca()
-        return [ax.plot(i) for i in j]
-    def __repr__(self):
-        pass
+        envelope = ax.fill_between(self.timeaxis, self.upper, self.lower, alpha=0.3)
+        line = ax.plot(self.timeaxis, self.bestestimate)
+        return [envelope, line] # should rather be a join
+    def __str__(self):
+        return '<{n} aggregated areas>'.format(n=self.pixels)
     def __add__(self, other):
-        raise NotImplementedError
+        if not isinstance(other, AggregateDynamics):
+            raise TypeError
+        new = AggregateDynamics.__new__()
+        new.pixels = self.pixels + other.pixels
+        assert len(self.timeaxis) == len(other.timeaxis)
+        assert all(self.timeaxis == other.timeaxis)
+        # TODO: trim/filter time series if operands not perfectly matched
+        new.timeaxis = self.timeaxis
+        new.upper = self.upper + other.upper
+        new.lower = self.lower + other.lower
+        new.bestestimate = self.bestestimate + other.bestestimate
+        return new
+
+class Naive(AggregateDynamics):
+    def compute(self):
+        self.bestestimate = self.masked.sum(axis=(1,2))
+    def plot(self, axes=None):
+         ax = axes or plt.gca()
+         return ax.plot(self.timeaxis, self.bestestimate,
+                        marker='.', alpha=0.7)
+class Broken(Naive):
+    def compute(self):
+        self.bestestimate = self.pixels * self.masked.mean(axis=(1,2))
+
+class NaiveBounds(AggregateDynamics):
+    def compute(self):
+        self.lower = self.masked.sum(axis=(1,2))
+        self.upper = self.pixels - (~self.masked).sum(axis=(1,2))
+    def plot(self, axes=None):
+        ax = axes or plt.gca()
+        n = len(self.timeaxis)
+        tt = np.vstack([self.timeaxis]*2)
+        ww = np.vstack([self.lower, np.zeros(n)])
+        dd = np.vstack([self.upper, np.ones(n)*self.pixels])
+        ax.plot(tt, ww, c='b', alpha=0.1)
+        ax.plot(tt, dd, c='r', alpha=0.1)
+        #ax.plot(tt, np.vstack([self.upper, self.lower]), c='k')
+        ax.plot(self.timeaxis, self.bestestimate, marker='.', linewidth=0, alpha=0.3, c='k')
 
 
 class ConstantBracket(AggregateDynamics):
-    pass
+    def compute(self):
+        mask = self.masked.mask
+        forward = self.masked.data.copy()
+        reverse = forward.copy()
+
+        epochs = len(self.timeaxis)
+
+        past = mask.cumsum(axis=0)
+        future = mask[::-1,...].cumsum(axis=0)[::-1,...]
+        unbound = (past == 0) | (future == 0)
+
+        # constant-value extrapolation
+        for i in range(1, epochs):
+            gaps = mask[i]
+            forward[i][gaps] = forward[i-1][gaps]
+        for i in range(epochs-1)[::-1]:
+            gaps = mask[i]
+            reverse[i][gaps] = reverse[i+1][gaps]
+
+        upper = forward | reverse | unbound
+        lower = forward & reverse & ~unbound
+
+        self.model = 0.5 * (upper + lower)
+
+        self.upper = upper.sum(axis=(1,2))
+        self.lower = lower.sum(axis=(1,2))
+
 
 class TelegraphProcess(AggregateDynamics):
     pass
+
+
+
+
