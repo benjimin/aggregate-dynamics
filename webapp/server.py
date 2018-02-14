d2aebdb2ec0cc75256fb2dc3977681ae94cfadd5
Ben Lewis <ben.lewis@ga.gov.au>
2018-02-15 00:01:31 +1100

    communicating with flask

--- webapp/server.py
+++ webapp/server.py
@@ -5,3 +5,9 @@ app = flask.Flask(__name__)
 def index():
     return flask.redirect(flask.url_for('static', filename='client.html'))
 
+@app.route('/plot', methods=['POST'])
+def receive():
+    data = flask.request.get_json()
+    assert data['type'] == 'Polygon'
+    print(data)
+    return repr(data['coordinates'])
