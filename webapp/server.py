746d8dcb6dc46da545b6ab2fadad2d8dcb2c15bc
Ben Lewis <ben.lewis@ga.gov.au>
2018-02-15 17:19:27 +1100

    snap to grid

--- webapp/server.py
+++ webapp/server.py
@@ -16,30 +16,32 @@ def receive():
     assert data['type'] == 'Polygon'
     poly = shapely.geometry.shape(data)
 
+    response = flask.jsonify(geom_to_grid(poly))
+    response.status_code = 200
+    return response
+
+def geom_to_grid(geom, snap=True):
     res = 100000
-    x0,y0,x1,y1 = np.asarray(poly.bounds) / res
+    x0,y0,x1,y1 = np.asarray(geom.bounds) / res
     x0,y0,x1,y1 = math.floor(x0), math.floor(y0), math.ceil(x1), math.ceil(y1)
-
     affine = rasterio.Affine(res, 0, x0*res, 0, -res, y1*res)
-    array = rasterio.features.rasterize(
-                [poly],
-                out_shape=(y1-y0, x1-x0),
-                transform=affine,
-                all_touched=True)
-    shapes = list(rasterio.features.shapes(array, mask=array, transform=affine))
-    assert len(shapes) == 1
-    exterior = shapes[0]
 
-    array2 = rasterio.features.rasterize(
-                [poly.exterior],
-                out_shape=(y1-y0, x1-x0),
-                transform=affine,
-                all_touched=True)
-    array[array2 != 0] = 0
-    shapes = list(rasterio.features.shapes(array, mask=array, transform=affine))
-    interiors = shapes
+    def rasterise(geom, all_touched=True):
+        return rasterio.features.rasterize([geom],
+                                           out_shape=(y1-y0, x1-x0),
+                                           transform=affine,
+                                           all_touched=all_touched)
+    def vectorise(array):
+        shapes = rasterio.features.shapes(array,
+                                          mask=array,
+                                          transform=affine)
+        return list(geom for (geom, value) in shapes)
 
-    z = flask.jsonify(interiors[0][0])
-    #z = flask.jsonify(exterior[0])
-    z.status_code = 200
-    return z
+    array = rasterise(geom, not snap)
+    polygons = vectorise(array)
+
+    # TODO: if len(..) == 0 i.e. tiny input region
+    if len(polygons) == 1:
+        return polygons[0]
+    else:
+        return dict(type='GeometryCollection', geometries=polygons)
